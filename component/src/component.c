de26ca2946b7995f553cad7c95f8e290ff385b26
ancil <ancilchandler@gmail.com>
2024-02-25 13:36:20 -0700

    changed wolfssl include paths

--- component/src/component.c
+++ component/src/component.c
@@ -28,10 +28,10 @@
 #include "ectf_params.h"
 #include "global_secrets.h"
 
-#include <wolfssl/options.h>
-#include <wolfssl/wolfcrypt/aes.h>
-#include <wolfssl/wolfcrypt/random.h>
-#include <wolfssl/wolfcrypt/sha256.h>
+#include <wolfssl/openssl/options.h>
+#include <wolfssl/openssl/aes.h>
+#include <wolfssl/openssl/random.h>
+#include <wolfssl/openssl/sha256.h>
 
 #ifdef POST_BOOT
 #include "led.h"
